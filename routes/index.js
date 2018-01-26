0733cb849c315ad28a1133f2b9f544ccbe99b09a
Mukul Sharma <mukulsharma257@gmail.com>
2018-01-26 16:14:41 +0530

    Add api interface

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,24 @@ router.post("/trade/", function (req, res, next) {
 
 })
 
+router.put("/trade/", function (req, res, next) {
+
+})
+
+router.delete("/trade/", function (req, res, next) {
+
+})
+
+route.get("/portfolio", function (req, res, next) {
+
+})
+
+router.get("/holdings", function (req, res, next) {
+
+})
+
+router.get("/returns", function (req, res, next) {
+
+})
+
 module.exports = router;
