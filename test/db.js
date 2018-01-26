1e47f318a7405a06657dd1864788770cf0235cb0
Mukul Sharma <mukulsharma257@gmail.com>
2018-01-26 17:04:47 +0530

    Add database tests

---
+++ test/db.js
@@ -0,0 +1,87 @@
+var expect = require('chai').expect;
+const db = require('../db');
+
+describe('openDatabase()', function () {
+  it ('should create database', function () {
+    return db.openDatabase().then(function (database) {
+      expect(database).to.not.equal(null);
+    }).catch (function (e) {
+      throw e;
+    });
+  })
+});
+
+describe('insert()', function () {
+  it ('should insert element in database ', function () {
+    return db.openDatabase().then(function (database) {
+      return db.insert(database, "test", {a: 1}).then(function (r) {
+
+        expect(r.result.n).to.be.equal(1);
+      }).catch(function (e) {
+        throw e;
+      })
+    }).catch (function (e) {
+      throw e;
+    })
+  })
+});
+
+describe('insertMany()', function () {
+  it ('should insert 3 element', function () {
+    return db.openDatabase().then(function (database) {
+      return db.insertMany(database, "test", [{a: 1}, {b: 1}, {c: 1}]).then(function (r) {
+
+        expect(r.result.n).to.be.equal(3);
+      }).catch(function (e) {
+        throw e;
+      })
+    }).catch (function (e) {
+      throw e;
+    })
+  })
+});
+
+describe('find()', function () {
+  it ('should find all elements', function () {
+    return db.openDatabase().then(function (database) {
+      return db.find(database, "test").then(function (r) {
+
+        expect(r.length).to.not.equal(0);
+      }).catch(function (e) {
+        throw e;
+      })
+    }).catch (function (e) {
+      throw e;
+    })
+  })
+});
+
+describe('remove()', function () {
+  it ('should remove one element', function () {
+    return db.openDatabase().then(function (database) {
+      return db.remove(database, "test", {a: 1}).then(function (r) {
+
+        expect(r.result.n).to.be.equal(1);
+      }).catch(function (e) {
+        throw e;
+      })
+    }).catch (function (e) {
+      throw e;
+    })
+  })
+});
+
+describe('update()', function () {
+  it ('should remove one element', function () {
+    return db.openDatabase().then(function (database) {
+      return db.update(database, "test", {a: 1}, {a: 2}).then(function (r) {
+
+        expect(r.result.n).to.be.equal(1);
+      }).catch(function (e) {
+        throw e;
+      })
+    }).catch (function (e) {
+      throw e;
+    })
+  })
+});
